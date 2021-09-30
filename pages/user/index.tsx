3b91accf66e24206fa1138fa682e3dc5ad843f13
Shea Belsky <sheabelsky@gmail.com>
2021-09-30 18:30:05 -0400

    Better readme and docs

--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -4,7 +4,9 @@ import { ssrUserPage } from "lib/ssr_user_page";
 export const getServerSideProps: GetServerSideProps = ssrUserPage;
 
 // When exporting a React component using this syntax, Superjson does not kick in
-// Replace the below export with the commented-out line, and it will work.
+// Replace the below export with the commented-out line, then comment out the "export {default}" line,
+// and it will work.
+
 // import User from "./user";
 // export default User;
 
